353889df0e0a22bcc7d616085315a5a3f1161e6f
Minh Huy <nnmhuy@apcs.vn>
2020-01-08 10:56:51 +0700

    hamming loss

---
+++ evaluation/clean_test.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('../preprocess/')
+import preprocess
+import pandas as pd
+
+test_labels = pd.read_csv('../data/test_labels.csv')
+test = pd.read_csv('../data/test.csv')
+
+# test = pd.concat([test, test_labels], axis=1, sort=False)
+test = test.merge(test_labels, how='outer').fillna(0)
+
+test = test[test['toxic'] >= 0]
+
+test_labels = test.loc[:, ['id', 'toxic','severe_toxic','obscene','threat','insult','identity_hate']]
+test = test.loc[:, ['id', 'comment_text']]
+
+print(test_labels.head())
+print(test.head())
+
+test_labels.to_csv('../data/clean_test_labels.csv',index=False)
+test_labels.to_csv('../data/clean_test.csv',index=False)
+
--- model/powerset-logistic-regression/powerset-logistic-regression.py
+++ model/powerset-logistic-regression/powerset-logistic-regression.py
@@ -7,13 +7,11 @@ data_path = '../../data/train.csv'
 
 train_data = preprocess.getPreprocessTrain(data_path)
 
-# choosing number of data
-# train_data = train_data.loc[:100, :]
+# train_data = train_data.loc[:10000, :]
 
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
@@ -30,14 +28,12 @@ for index, column in enumerate(train_data.columns[2:-1]):
   train_data['label'] = train_data['label'] | train_data[column].apply(lambda x: (x << index))
 
 train_data = train_data.loc[:, ['comment_text', 'label']]
-# filter 0 label
 train_data = train_data[train_data['label'] > 0]
 print(train_data.head())
 
 # vectorize text
 print('Vectorizing text ...')
-# vectorizer = CountVectorizer(min_df=1)
-vectorizer = TfidfVectorizer(binary=True, use_idf=True)
+vectorizer = CountVectorizer(min_df=1)
 x = vectorizer.fit_transform(train_data['comment_text']).toarray()
 y = train_data['label']
 
@@ -50,41 +46,15 @@ scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-# Fitting Random Forest Classification to the Training set
-print('Training ...')
-classifier = RandomForestClassifier(n_estimators = 20, criterion = 'entropy', random_state = 42)
+# using Label Powerset
+from sklearn.linear_model import LogisticRegression
+from skmultilearn.problem_transform import LabelPowerset
+# initialize label powerset multi-label classifier
+classifier = LogisticRegression()
+# train
 classifier.fit(x_train, y_train)
-
-# Predicting the Test set results
-print('Get predictions ...')
-y_pred = classifier.predict(x_test)
-y_test_np = y_test.to_numpy()
-
-# Making the Confusion Matrix
-print(pd.crosstab(y_test, y_pred, rownames=['Actual Label'], colnames=['Predicted Label']))
-
+# predict
+predictions = classifier.predict(x_test)
 # accuracy
-print("Accuracy = ", accuracy_score(y_test, y_pred))
+print("Accuracy = ",accuracy_score(y_test,predictions))
 print("\n")
-
-# use to predict
-print('Testing ...')
-test_data_path = '../../data/clean_test.csv'
-test_data = preprocess.getPreprocessTrain(data_path)
-
-test_x = vectorizer.transform(test_data['comment_text']).toarray()
-
-output = classifier.predict(test_x)
-output_df = pd.DataFrame({'label': output[:]})
-
-
-label_list = ['toxic','severe_toxic','obscene','threat','insult','identity_hate']
-
-for index, label in enumerate(label_list):
-  output_df[label] = 0
-  output_df[label] = (output_df['label'].apply(lambda x: (x >> index) & 1))
-
-del output_df['label']
-output_df = pd.concat([test_data['id'], output_df], axis=1, sort=False)
-
-output_df.to_csv('./powerset-random-forest.csv',index=False)
