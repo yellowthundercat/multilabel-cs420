82e2b154148087913090b475ca8b27e5a5e412a9
Huu-Duc <yellowthundercat@gmail.com>
2020-01-08 14:49:10 +0700

    binary

--- model/binary-bayesian/UnigramLanguageModel.py
+++ model/binary-bayesian/UnigramLanguageModel.py
@@ -1,36 +1,66 @@
 import math, collections
 powerSet = 6
 thresh_hold = 0.1
+listLabel = ['toxic','severe_toxic','obscene','threat','insult','identity_hate']
 
 class UnigramLanguageModel:
 
   def __init__(self, corpus):
-    self.unigramCounts = [collections.defaultdict(lambda: 0)] * powerSet
-    self.total = [0] * powerSet
+    self.unigramCounts = [collections.defaultdict(lambda: 0)] * 7
+    self.nonInUnigramCounts = [collections.defaultdict(lambda: 0)] * 7
+    self.total = [0] * 7
+    self.p_label = [0] * 7
     self.train(corpus)
 
   def train(self, corpus):
-    for index in range(len(corpus['comment_text'])):
+    for index in range(min(10000, len(corpus['comment_text']))):
+      if (index % 1000) == 0: 
+        print(index/1000)
       sentence = corpus['comment_text'][index]
-      for token in sentence:
-        for column in range(0, powerSet):
-          if corpus[column + 1][index] == 1:
+      self.p_label[6] += 1
+      for column in range(0, len(listLabel)):
+        label = listLabel[column]
+        if corpus[label][index] == 1:
+          self.p_label[column] += 1
+        for token in sentence:
+          if column == 0:
+            self.unigramCounts[6][token] = self.unigramCounts[6][token] + 1
+            self.total[6] += 1
+          
+          self.total[column] += 1
+          if corpus[label][index] == 1:
             self.unigramCounts[column][token] = self.unigramCounts[column][token] + 1
-            self.total[column] += 1
+          else:
+            self.nonInUnigramCounts[column][token] += 1
     
+    for i in range(6):
+      self.p_label[i] = float(self.p_label[i]) / self.p_label[6]
     print(self.total)
   
   def score(self, sentence):
-    score = [0.0] * powerSet
-    for i in range(0, powerSet):
+    YesScore = [0.0] * powerSet
+    NoScore = [0.0] * powerSet
+    for i in range(0, len(listLabel)):
+      label = listLabel[i]
       for token in sentence:
-        count = self.unigramCounts[i][token]
-        score[i] += math.log(count + 1)
-        score[i] -= math.log(self.total[i] + 70000)
+        countYes = self.unigramCounts[i][token]
+        countNo = self.nonInUnigramCounts[i][token]
+        # score[i] += float(count + 1) / float(self.total[i] + 70000) * self.p_label[i] 
+        YesScore[i] += math.log(countYes + 1)
+        YesScore[i] -= math.log(self.total[i] + 70000)
+
+        NoScore[i] += math.log(countNo + 1)
+        NoScore[i] -= math.log(self.total[i] + 70000)
+      
+      YesScore[i] += math.log(self.p_label[i])
+      NoScore[i] += math.log(1.0 - self.p_label[i])
+      
+    # print(score)
+    # print(logscore)
     
     maxLabel = [0] * powerSet
     for i in range(0, powerSet):
-      if (score[i] > thresh_hold):
+      if (YesScore[i] > NoScore[i]):
         maxLabel[i] = 1
     
     return maxLabel
--- model/binary-bayesian/binary-bayesian.py
+++ model/binary-bayesian/binary-bayesian.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 sys.path.append('../../preprocess/')
 import preprocess
+import numpy as np
 from UnigramLanguageModel import UnigramLanguageModel
 
 data_path = '../../data/small_train.csv'
@@ -10,6 +11,7 @@ test_path = '../../data/test.csv'
 test_label_path = '../../data/test_labels.csv'
 
 train_data = preprocess.getPreprocessTrain(data_full_path)
+#train_data = preprocess.getPreprocessTrain(data_path)
 # test_data = preprocess.getPreprocessTest(test_path)
 
 # insert column label
@@ -32,14 +34,15 @@ countRight = 0
 countDifZero = 0
 ans_predict = []
 ans_actual = []
+ArrayLabel = np.asarray(testLabel)
 for i in range(0, len(testLabel['id'])):
   if testLabel['toxic'][i] != -1:
     testSentence = testData['comment_text'][i]
     testSentence = preprocess.clean_text(testSentence)
     predict = unigram.score(testSentence)
     print(predict)
-    print(testLabel[1:][i])
+    print(ArrayLabel[i])
     count += 1
-    if count > 2000:
+    if count > 200:
       break
 # print (count, countRight, countDifZero)
